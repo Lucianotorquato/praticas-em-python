bdf1d4bb9f77303fc7c5e50abe89638c117667bc
Luciano Alegranci Torquato de Araujo <lucianotorquato81@gmail.com>
2022-08-08 23:56:49 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -173,6 +173,54 @@ else:
 print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(preço, total))
 
 
+#EX 45
+from random import randint
+from time import sleep 
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''Suas Opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL 
+[ 2 ] TESOURA''')
+jogador = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
+print ('-='* 11)
+print('Computador jogou {}'.format(itens[computador]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('-='*11)
+if computador == 0:
+    if jogador ==0: 
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCEU')
+    elif jogador == 2:
+        print('COMPUTADOR VENCEU')
+    else:
+        print('JOGADA INVÁLIDA!')
+elif computador == 1:
+    if jogador == 0:
+        print('COMPUTADOR VENCEU')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCEU')
+    else: 
+        print('JOGADA INVÁliDA!')
+elif computador == 2:
+    if jogador == 0:
+        print('JOGADOR VENCEU')
+    elif jogador == 1:
+        print('COMPUTADOR VENCEU')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVÁLIDA!')
+
+
 
 
 
